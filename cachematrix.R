486a38a573c8ae04ff8961ec91c959d2c686abc4
Abhimanyu <banerjee.abhimanyu3@gmail.com>
2016-12-25 19:45:06 -0500

    add comments to improve documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
 
-## Write a short comment describing this function
+## Coursera - R Programming Course
+## Author: Abhimanyu Banerjee
+## Date: 12/25/2016
 
+## File Description:
+
+# This script demonstrates the power of lexical scoping. Lexical Scoping is used
+# to create a function constructor to define the special caching behaviour of 
+# the matrix whose inverse is to be calculated. The function constructor returns
+# a list of four functions:
+# 1. set - caches the value of the matrix and initializes its inverse as null
+# 2. get - gets the cached value of the matrix
+# 3. setInv - caches the inverse (passed to it as an argument) of the matrix
+# 4. getInv - gets the cached inverse of the matrix
+ 
+###############################################################################
+
+## This is a contructor function that sets up the caching behaviour of the
+## special matrix whose inverse is to be computed (see function after this one)
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
+    ## Return a list of getter-setter functions for the matrix and its inverse
+    inv <- NULL 
     set <- function(matr){
         x <<- matr
         inv <<- NULL
@@ -16,8 +33,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of a matrix. The function first checks if 
+## the inverse of the matrix was cached previously. If it was, it returns that 
+## value, otherwise caches the matrix and its computed inverse for future use.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     matrInv <- x$getInv()
